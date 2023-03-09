80583b40750aa00b14eb8225846e1b35c086ddca
vermaarun1995 <vermaarun1995@gmail.com>
2023-03-09 20:42:34 +0530

    Admin Commit

---
+++ Veelki.Admin/Veelki.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Veelki.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
