1acc039d8cbbb56cc5366242d48609845424b422
vermaarun1995 <vermaarun1995@gmail.com>
2023-03-14 21:02:39 +0530

    Remove

--- veelki.user/RB444UserAngular/src/app/helpers/thousand-suffixes.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ThousandSuffixesPipe } from './thousand-suffixes.pipe';
-
-describe('ThousandSuffixesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ThousandSuffixesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- veelki.user/RB444UserAngular/src/app/models/stackLimit.ts
+++
@@ -1,4 +0,0 @@
-export interface StackLimit {
-    id : number;
-    stakeLimitPrice : number;  
-  }
